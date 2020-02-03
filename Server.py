980a8b39ef880bd14b956f6b50c4f916080e70b7
Corey Coleson <55822260+CColeson@users.noreply.github.com>
2020-02-03 01:32:51 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,72 +1,66 @@
 import socket
-import pyglet
-from GameObjects.Player import MyPlayer
-from GameObjects import GameObject
-from pyglet.window import key
-from pyglet.window import mouse
-import pickle
+import threading
 import random
+from GameObjects.Player import ServerPlayer
+import pickle
 
-def connect():
-    name = input('Enter your name: a unique one may be assigned\n')
+def addToList(msg):
+    global objectList
 
-    s = socket.socket()
-    s.connect((HOST,PORT))
-    s.send(('00 '+name).encode())
+    name = msg[3:]
+    while name in objectList: #while the name is not unique
+        #add a random string to the end of the name to ensure uniqueness (its either this or hashing)
+        name = objectList[name] + '_' + str(random.randrange(1000,9999))
+        
+    objectList[name] = ServerPlayer(name,0,0,'GameObjects/Player/player.png')#TODO: Server representation of object, or game representation, that way it could be pickled
+    conn.send(("00 "+name).encode())
 
-    name = s.recv(1024).decode()
-    if name[0:2] == '00':
-        name = name[3:]
-        print('Connected Successfully as %s!'%name)
-    else:
-        input('Something went wrong, press "enter" to try again')
-        s,name = connect()
+def recievePlayerObject(msg):
+    global objectList
 
-    return s,name
+    player = msg[3:].split(' ')
+    player = ServerPlayer(player[0],player[1],player[2],player[3])
+    objectList[player.name] = player
+
+def onClientConnect(conn,addr):
+    name = '' #unique client name
+    while True:
+
+        msg = conn.recv(1024).decode()
+
+        if msg[0:2] == '00':
+            addToList(msg)
+
+        if msg[0:2] == '01':
+            recievePlayerObject(msg)
+
+        msg = '01 '
+        for o in objectList:
+            msg += '%s %s %s %s\n'%(objectList[o].name,objectList[o].x,
+                                    objectList[o].y,objectList[o].imagePath)
+        conn.send(msg.encode())
+    conn.close()
+    del objectList[name]
+
+
+
+
+
 
 if __name__ == '__main__':
-    
-    HOST = 'ENTER YOUR IP HERE'
+    HOST = ''
     PORT = 50007
-    s,name = connect()
 
-    window = pyglet.window.Window(height=1000,width=1000)
-    keys = key.KeyStateHandler()
-    window.push_handlers(keys)
+    s = socket.socket()
 
-    player = MyPlayer(name,random.randrange(1000),random.randrange(1000),keys)
-    #s.send(('01 %s %s %s %s'%(player.name,player.x,player.y,player.imagePath)).encode())
+    objectList = {} #list of connected players
 
-    drawObjects = {}
-    
-    def update(time):
-        window.clear()
+    s.bind((HOST,PORT))
+    s.listen()
 
-        msg = s.recv(1024).decode()
-        if msg[0:2] == '01':
-            msg = msg[3:]
-            obs = msg.split('\n')
-            for o in obs:
-                o = o.split()
-                if len(o) == 0: continue
-                if o[0] not in drawObjects:
-                    drawObjects[o[0]] = GameObject(o[0],int(o[1]),int(o[2]),o[3],drawLabels=True)
-                else:
-                    obj = drawObjects[o[0]]
-                    obj.x = int(o[1])
-                    obj.y = int(o[2])
-                    obj.imagePath = o[3]
-                
-                
-        player.drawSelf()
-        
-        for i in drawObjects:
-            if drawObjects[i].name != player.name:
-                drawObjects[i].drawSelf()
-
-        msg = ('01 %s %s %s %s'%(player.name,player.x,player.y,player.imagePath)).encode()
-        s.send(msg)
-
-    pyglet.clock.schedule_interval(update,1/60)#1/60
-    pyglet.app.run()
-    s.close()
+    threads = []#thread list, stores all connections to server
+    while True:
+        conn,addr = s.accept()
+        t = threading.Thread(target=onClientConnect,args=(conn,addr))
+        t.start()
+        threads.append(t)
